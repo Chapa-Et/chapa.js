7c67b3db1f4d415f57edee8f0ecfd6f7818196e6
fuadhy <fuadmail3@gmail.com>
2022-08-19 08:29:39 +0300

    changes

--- README.md
+++ README.md
@@ -11,6 +11,18 @@ const Chapa = require('chapa')
 
 const myChapa = new Chapa('secret-key')
 
+const customerInfo =  {
+  'amount': '100',
+  'currency': 'ETB',
+  'email': 'abebe@bikila.com',
+  'first_name': 'Abebe',
+  'last_name': 'Bikila',
+  'tx_ref': 'tx-myecommerce12345',
+  'callback_url': 'https://chapa.co',
+  'customization[title]': 'I love e-commerce',
+  'customization[description]': 'It is time to pay'
+}
+
 // intializing transaction
 myChapa.initialize({ ...customerInfo }).then(response => {
     console.log(response) // if success json response
@@ -27,4 +39,5 @@ myChapa.verify('txn-reference').then(response => {
 
 // async/await
 let response = await myChapa.verify('txn-reference')
+
 ```
--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 const Chapa = require('./index')
 
-let myChapa = new Chapa('12345')
-myChapa.initialize({
-    'amount': '100',
-    'currency': 'ETB',
-    'email': 'abebe@bikila.com',
-    'first_name': 'Abebe',
-    'last_name': 'Bikila',
-    'tx_ref': 'tx-myecommerce12345',
-    'callback_url': 'https://chapa.co',
-    'customization[title]': 'I love e-commerce',
-    'customization[description]': 'It is time to pay'
-}).then(d => console.log('ddd', d))
-.catch(e => console.log(e))
+let myChapa = new Chapa('CHASECK_TEST-OeEWekN3KjoBTSwkkDdds9nZ8PhMBrkk')
+// myChapa.initialize({
+//     'amount': '100',
+//     'currency': 'ETB',
+//     'email': 'abebe@bikila.com',
+//     'first_name': 'Abebe',
+//     'last_name': 'Bikila',
+//     'tx_ref': 'tx-x12345',
+//     'callback_url': 'https://chapa.co',
+//     'customization[title]': 'I love e-commerce',
+//     'customization[description]': 'It is time to pay'
+// }).then(d => console.log('ddd', d))
+// .catch(e => console.log(e))
 
-// myChapa.verify('e3e3e3e3e').then(d => console.log(d)).catch(e => console.log(e))
+myChapa.verify('tx-x12345').then(d => console.log(d)).catch(e => console.log(e))
