6646ab30d73423c90e4fab770e8236eebd103efe
fuadhy <fuadmail3@gmail.com>
2022-09-20 17:43:33 +0300

    reverted to prototypes and added options

--- test.js
+++ test.js
@@ -1,17 +1,35 @@
-const Chapa = require('./index')
+const Chapa = require('./lib/chapa')
 
-let myChapa = new Chapa('CHASECK_TEST-OeEWekN3KjoBTSwkkDdds9nZ8PhMBrkk')
-// myChapa.initialize({
-//     'amount': '100',
-//     'currency': 'ETB',
-//     'email': 'abebe@bikila.com',
-//     'first_name': 'Abebe',
-//     'last_name': 'Bikila',
-//     'tx_ref': 'tx-x12345',
-//     'callback_url': 'https://chapa.co',
-//     'customization[title]': 'I love e-commerce',
-//     'customization[description]': 'It is time to pay'
-// }).then(d => console.log('ddd', d))
-// .catch(e => console.log(e))
+let myChapa = new Chapa('secret-key')
 
-myChapa.verify('tx-x12345').then(d => console.log(d)).catch(e => console.log(e))
+const customerInfo =  {
+    amount: '100',
+    currency: 'ETB',
+    email: 'abebe@bikila.com',
+    first_name: 'Abebe',
+    last_name: 'Bikila',
+    // tx_ref: 'tx-x12345',
+    callback_url: 'https://chapa.co', // your callback URL
+    customization: {
+        title: 'I love e-commerce',
+        description: 'It is time to pay'
+    }
+}
+
+myChapa.initialize(customerInfo, { autoRef: true }).then(response => {
+    /*
+    response:
+        {
+        message: 'Hosted Link',
+        status: 'success' || 'failed',
+        data: {
+            checkout_url: 'https://checkout.chapa.co/checkout/payment/:token'
+        },
+        tx_ref: 'generated-token' // this will be the auto generated reference
+        }
+    */
+    console.log(response)
+    // saveReference(response.tx_ref)
+}).catch(e => console.log(e)) // catch errors
+
+// myChapa.verify('5b1472b1-4b28-475c-8fa9-8bf2242fea9f').then(d => console.log(d)).catch(e => console.log(e))
